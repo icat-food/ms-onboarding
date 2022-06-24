ca74fd50253b752d1c2d19686f498b29504f65ca
Vandeilson L Nobre <62423423+Vandeilsonln@users.noreply.github.com>
2022-06-24 13:47:20 -0300

    Initial commit
    Criado arquivos iniciais

---
+++ src/main/kotlin/com/icat/orboarding/OrboardingApplication.kt
@@ -0,0 +1,11 @@
+package com.icat.orboarding
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OrboardingApplication
+
+fun main(args: Array<String>) {
+	runApplication<OrboardingApplication>(*args)
+}
---
+++ src/test/kotlin/com/icat/orboarding/OrboardingApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.icat.orboarding
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OrboardingApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
