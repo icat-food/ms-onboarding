93e070cb0008517c70e06830ba2eefaec4526a25
kaikeventura <kaike.gnr@hotmail.com>
2022-06-27 23:38:28 -0300

    Ajustado ddls e criado campos de criação e atualização

--- src/main/resources/db/migration/V3__create_restaurant_table.sql
+++ src/main/resources/db/migration/V3__create_restaurant_table.sql
@@ -1,9 +1,11 @@
 CREATE TABLE restaurant (
     id VARCHAR(50) NOT NULL,
     name VARCHAR(100) NOT NULL,
-    cnpj VARCHAR(15) NOT NULL UNIQUE,
+    cnpj VARCHAR(14) NOT NULL UNIQUE,
     image_url VARCHAR(255) NOT NULL UNIQUE,
     user_id VARCHAR(50) NOT NULL,
+    created_at DATETIME NOT NULL,
+    updated_at DATETIME NULL,
 
     CONSTRAINT pk_restaurant_id PRIMARY KEY (id),
     CONSTRAINT fk_restaurant_user_id FOREIGN KEY (user_id) REFERENCES user (id)
--- src/main/resources/db/migration/V4__create_delivery_table.sql
+++ src/main/resources/db/migration/V4__create_delivery_table.sql
@@ -1,9 +1,11 @@
 CREATE TABLE delivery (
     id VARCHAR(50) NOT NULL,
     full_name VARCHAR(100) NOT NULL,
-    cpf VARCHAR(12) NOT NULL UNIQUE,
+    cpf VARCHAR(11) NOT NULL UNIQUE,
     image_url VARCHAR(255) NOT NULL UNIQUE,
     user_id VARCHAR(50) NOT NULL,
+    created_at DATETIME NOT NULL,
+    updated_at DATETIME NULL,
 
     CONSTRAINT pk_delivery_id PRIMARY KEY (id),
     CONSTRAINT fk_delivery_user_id FOREIGN KEY (user_id) REFERENCES user (id)
