84c336dad142bae4540613824b3a703b454a8a03
kaikeventura <kaike.gnr@hotmail.com>
2022-07-02 17:39:34 -0300

    Criado uma migration para incluir a constraint unique na coluna de fk

---
+++ src/test/kotlin/com/icat/orboarding/user/adapters/outbound/persistence/mysql/RestaurantPersistenceTest.kt
@@ -0,0 +1,16 @@
+package com.icat.orboarding.user.adapters.outbound.persistence.mysql
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class RestaurantPersistenceTest {
+
+    @Test
+    fun cnpjAlreadyRegistered() {
+    }
+
+    @Test
+    fun createRestaurant() {
+    }
+}
