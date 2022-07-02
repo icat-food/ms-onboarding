1461830554a2dc5ac7041d4b859731bff8f9b5c4
kaikeventura <kaike.gnr@hotmail.com>
2022-07-01 21:41:56 -0300

    Revert "Revert "Revert "Desenvolvido camada de persistência para buscar um user"""
    This reverts commit 4214c9ffb90162ed33c93b94545627fc1a03f0cc.

--- src/main/kotlin/com/icat/orboarding/user/adapters/outbound/persistence/mysql/repositories/UserRepository.kt
+++ src/main/kotlin/com/icat/orboarding/user/adapters/outbound/persistence/mysql/repositories/UserRepository.kt
@@ -3,10 +3,8 @@ package com.icat.orboarding.user.adapters.outbound.persistence.mysql.repositorie
 import com.icat.orboarding.user.adapters.outbound.persistence.mysql.entities.UserEntity
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
-import java.util.Optional
 
 @Repository
 interface UserRepository : JpaRepository<UserEntity, String> {
     fun existsByEmailContainsIgnoreCase(email: String): Boolean
-    fun findByEmailIgnoreCase(email: String): Optional<UserEntity>
 }
