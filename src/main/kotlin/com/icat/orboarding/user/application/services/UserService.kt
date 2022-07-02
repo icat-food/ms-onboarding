75ed0b48479f57d1f5e0a57d1e1a7cb4bbd7f7be
kaikeventura <kaike.gnr@hotmail.com>
2022-07-01 21:12:19 -0300

    Desenvolvido camada de serviço para buscar um user

--- src/main/kotlin/com/icat/orboarding/user/application/services/UserService.kt
+++ src/main/kotlin/com/icat/orboarding/user/application/services/UserService.kt
@@ -2,6 +2,7 @@ package com.icat.orboarding.user.application.services
 
 import com.icat.orboarding.user.application.domain.UserDomain
 import com.icat.orboarding.user.application.exceptions.EmailAlreadyRegisteredException
+import com.icat.orboarding.user.application.exceptions.UserNotFoundException
 import com.icat.orboarding.user.application.ports.inbound.UserServicePort
 import com.icat.orboarding.user.application.ports.outbound.UserPersistencePort
 
@@ -14,6 +15,11 @@ class UserService(private val userPersistencePort: UserPersistencePort) : UserSe
 
         return userPersistencePort.createUser(userDomain).removePassword()
     }
+
+    override fun getUser(email: String): UserDomain =
+        userPersistencePort.getUser(email)
+            .map { it }
+            .orElseThrow { throw UserNotFoundException("User with email $email not found") }
 
     private fun UserDomain.removePassword(): UserDomain =
         copy(password = null)
