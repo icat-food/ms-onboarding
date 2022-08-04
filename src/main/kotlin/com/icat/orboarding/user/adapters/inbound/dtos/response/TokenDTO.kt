746d7f2816feb3d387fc0814f6ae884887e744da
Vandeilson L Nobre <62423423+Vandeilsonln@users.noreply.github.com>
2022-08-03 23:25:47 -0300

    Update src/main/kotlin/com/icat/orboarding/user/adapters/inbound/dtos/response/TokenDTO.kt
    Co-authored-by: kaikeventura <44590259+kaikeventura@users.noreply.github.com>

--- src/main/kotlin/com/icat/orboarding/user/adapters/inbound/dtos/response/TokenDTO.kt
+++ src/main/kotlin/com/icat/orboarding/user/adapters/inbound/dtos/response/TokenDTO.kt
@@ -2,7 +2,6 @@ package com.icat.orboarding.user.adapters.inbound.dtos.response
 
 import com.icat.orboarding.user.application.domain.TokenDomain
 
-data class TokenDTO(var token: String) {
 }
 
 fun TokenDomain.toTokenDTO(): TokenDTO =
